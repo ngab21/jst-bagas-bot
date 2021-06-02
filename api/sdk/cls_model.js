0b771ce9a12598b8ff6fdb361019e3b77424dad9
ngab21 <84616551+ngab21@users.noreply.github.com>
2021-06-02 11:47:09 +0700

    Update cls_model.js

--- api/sdk/cls_model.js
+++ api/sdk/cls_model.js
@@ -31,7 +31,7 @@ async function classyfy(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/zendi014/jst_service/main/public/cls_model/model.json';
+        const path = 'https://raw.githubusercontent.com/ngab21/jst-bagas-bot/main/public/ex_model/model.json;
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
